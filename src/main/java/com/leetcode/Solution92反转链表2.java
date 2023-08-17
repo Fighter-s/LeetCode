5d91c048368d9f44478cd2d0ecef84fa769335e1
shiguangpeng <shiguangpeng@51shebao.com>
2023-08-17 21:57:01 +0800

    反转链表2

--- src/main/java/com/leetcode/Solution92反转链表2.java
+++ src/main/java/com/leetcode/Solution92反转链表2.java
@@ -3,6 +3,26 @@ package com.leetcode;
 public class Solution92反转链表2 {
 
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        return null;
+        ListNode node = new ListNode(0);
+        node.next = head;
+
+        ListNode start = node;
+        ListNode end = node.next;
+
+        // 找到start和end的节点
+        for(int i = 0 ; i< left-1;i++){
+            start = start.next;
+            end = end.next;
+        }
+
+        // 采用头插法
+        for(int i=0;i < right-left;i++){
+            ListNode temp = end.next;
+            end.next = end.next.next;
+            temp.next = start.next;
+            start.next = temp;
+        }
+
+        return node.next;
     }
 }
